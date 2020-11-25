42d1aa686b63b5fc0a7bf500957d286349517268
CraigSmith789 <craig789@gmail.com>
2020-11-24 21:00:54 -0500

    commit tree 9841bef89e9afc284e988c182144f6293d4c270d

---
+++ db/migrate/007_add_day_season_genre_to_shows.rb
@@ -0,0 +1,8 @@
+class AddDaySeasonGenreToShows < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :shows, :day, :string, options = {}
+    add_column :shows, :season, :string, options = {}
+    add_column :shows, :genre, :string, options = {}
+  end
+end
